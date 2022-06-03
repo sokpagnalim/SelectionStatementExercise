4369f9241ebad441de0a8c21c9cf187290b29f52
Sokpagna Lim <sokpagnalim@gmail.com>
2022-06-02 19:17:03 -0700

    Selection Statement Exercise 1

---
+++ SelectionStatementExercise/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SelectionStatementExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // your favorite number guessing games
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+           
+                Console.WriteLine("Pick a number from 1 -1000.");
+                var userInput = int.Parse(Console.ReadLine());
+
+
+                if (userInput < favNumber)
+                {
+                    Console.WriteLine($"{userInput} is too low");
+                }
+                else if (userInput > favNumber)
+                {
+                    Console.WriteLine($"{userInput} is too high");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+          
+        }
+    }
+}
